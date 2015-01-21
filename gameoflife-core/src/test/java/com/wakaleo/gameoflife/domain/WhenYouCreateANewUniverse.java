7673a40ba0d8499f4344b223f37eabdc051b4c6c
Dado <dado@hotmail.com>
2015-01-21 15:15:30 +0000

    Fixed and ReEnabled CreateUniverse TCs again

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateANewUniverse.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateANewUniverse.java
@@ -12,11 +12,11 @@ import static org.hamcrest.Matchers.*;
 
 public class WhenYouCreateANewUniverse {
 
-    private static final String NEW_LINE = "_"; //System.getProperty("line.separator");
+    private static final String NEW_LINE = System.getProperty("line.separator");
 
     public static final String EMPTY_GRID = "..." + NEW_LINE + "..." + NEW_LINE + "..." + NEW_LINE + "";
 
-    /*    @Test
+        @Test
     public void aNewUniverseShouldContainOnlyDeadCells() {
         Universe theUniverse = new Universe();
         String currentGrid = theUniverse.getGrid();
@@ -139,6 +139,6 @@ public class WhenYouCreateANewUniverse {
 
         assertThat(theUniverse.getCells(), is(expectedCells));
     }
-    */
+    
 
 }
